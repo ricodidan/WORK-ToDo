7d78266453e32931a62a4e1257e3cacc7a7c19d2
Enrico_Didan <ricodidan@gmail.com>
2017-01-21 10:35:50 +0700

    First AppIntro Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/worktodo/CustomAnimation.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.worktodo;
+
+/**
+ * Created by Enrico_Didan on 21/01/2017.
+ */
+
+public class CustomAnimation {
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/worktodo/IntroActivity.java
@@ -0,0 +1,41 @@
+package id.sch.smktelkom_mlg.worktodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addSlide(SampleSlide.newInstance(R.layout.slide_1));
+        addSlide(SampleSlide.newInstance(R.layout.slide_2));
+        addSlide(SampleSlide.newInstance(R.layout.slide_3));
+
+        setFadeAnimation();
+    }
+
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    /*@Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }*/
+}
